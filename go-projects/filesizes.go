3b007ae4f1c1d955f07e192f49a8baaa8e39f8af
Tommy Gildseth <tommy@gildseth.com>
2015-02-17 20:01:06 +0100

    Switch calculation of unit index

--- go-projects/filesizes.go
+++ go-projects/filesizes.go
@@ -1,64 +1,64 @@
 package main
 
 import (
-	"fmt"
-        "path/filepath"
-        "os"
-        "math"
+  "fmt"
+  "path/filepath"
+  "os"
+  "math"
 
 )
 
 func main() {
-        var sizeRange [64]int
-        var maxSize int = 0
+  var sizeRange [64]int
+  var maxSize int = 0
 
-        if len(os.Args) < 2 {
-                fmt.Println("No path specified")
-                os.Exit(1)
-        }
+  if len(os.Args) < 2 {
+    fmt.Println("No path specified")
+    os.Exit(1)
+  }
 
-        _, err := os.Stat(os.Args[1])
-        if os.IsNotExist(err) {
-                fmt.Printf("%s does not exist\n", os.Args[1])
-                os.Exit(1)
-        }
-        
-        filepath.Walk(
-                os.Args[1],
-                func(path string, f os.FileInfo, err error) error {
-                        if !f.IsDir() {
-                                tmp := int(findSize(f.Size(), 0))
-                                if maxSize < tmp { maxSize = tmp }
-                                sizeRange[tmp]++
-                        }
-                        return nil
-                },
-        )
+  _, err := os.Stat(os.Args[1])
+  if os.IsNotExist(err) {
+    fmt.Printf("%s does not exist\n", os.Args[1])
+    os.Exit(1)
+  }
 
-        formatTable(sizeRange, maxSize)
+  filepath.Walk(
+    os.Args[1],
+    func(path string, f os.FileInfo, err error) error {
+      if !f.IsDir() {
+        tmp := int(findSize(f.Size(), 0))
+        if maxSize < tmp { maxSize = tmp }
+        sizeRange[tmp]++
+      }
+      return nil
+    },
+  )
+
+  formatTable(sizeRange, maxSize)
 }
 
 
 func findSize(number int64, exp float64) float64 {
-        if number < (1024*int64(math.Pow(2, exp))) || number == 64 {
-                return exp
-        }
-  
-        return findSize(number, exp+1)
+  if number < (1024*int64(math.Pow(2, exp))) || number == 64 {
+    return exp
+  }
+
+  return findSize(number, exp+1)
 }
 
 func formatTable(sizeRange [64]int, maxSize int) {
-        for i := 0; i <= maxSize; i++ {
-                fmt.Printf("# <%6s: %d\n", formatNum(math.Pow(2,float64(i)), 0), sizeRange[i])
-        }
-}
+  units := []string{"KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"}
 
-func formatNum(size float64, unit int) string {
-        units := []string{"KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"}
-        if size >= 1024 {
-                return formatNum(size / 1024, unit+1)
-        }
+  for i := 0; i <= maxSize; i++ {
+    size := math.Pow(2,float64(i))
+    index := math.Floor(math.Log(size) / math.Log(1024))
 
-        return fmt.Sprintf("%d%s", int(size), units[unit])
-        
+    fmt.Printf(
+      "# <%4d%s: %d\n",
+      int(size/math.Pow(1024,index)),
+      units[int(index)],
+      sizeRange[i],
+    )
+  }
 }
